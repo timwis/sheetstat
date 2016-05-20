00b44aa8e08562601c1bb4a803bb127d391e0e7c
Lauren Ancona <lauren.ancona@phila.gov>
2016-05-20 15:01:07 -0400

    Highlight selected polygon on map
    Closes #16, thanks to @rbrtmrtn

--- scripts/planning-districts.js
+++ scripts/planning-districts.js
@@ -29,47 +29,77 @@
     maxZoom: 16
   }).addTo(map)
   
+  // define polygon default styles
   var districtStyle = {
     "color": "#58c04d",
     "weight": 2,
     "opacity": 0.85
   };
-
   
   // Add districts layer
   var districtsLayer = L.geoJson(districtsGeojson, {
-    style: districtStyle,
+    style: function (feature){
+      var style = {
+        "color": "#58c04d",
+        "weight": 2,
+        "opacity": 0.85
+      };
+      // make inital polygon style match default value 
+        // TODO: this could be refactored to simply check 
+        // value of dropdown on load
+      if (feature.properties.DIST_NAME == 'Central'){
+        style.color = '#2176d2'
+      }
+      return style;
+    },
+    // Create label popup on hover
     onEachFeature: function (feature, layer){
       if (feature.properties.DIST_NAME) {
-        layer.bindLabel(feature.properties.DIST_NAME, { noHide: true });
+        layer.bindLabel(feature.properties.DIST_NAME, { noHide: true });          
       }
     }
   }).addTo(map)
+  
+  // Default to Central Planning District
+//  map.on('load', function () {
+//    planningDistrictDropdown.setAttribute('value', 'Central Northeast')
+//  }); 
 
   // When a district is clicked on the map, update the document to reflect its indicators
   districtsLayer.on('click', function (event) {
     var districtName = event.layer.feature.properties.DIST_NAME 
+    console.log(event.layer);
     updateIndicators(districtName)
-
+    
     // Set dropdown to the clicked planning district
+    // and reset polygon style when another is clicked
     planningDistrictDropdown.value = districtName
+    var highlight = {
+      'color': '#2176d2',
+      'weight': 2,
+      'opacity': 1
+    };
+    districtsLayer.setStyle(districtStyle);
+    event.layer.setStyle(highlight);
   })
-
+  // update the numeric values in right column when a new
+  // district is selected
   function updateIndicators (districtName) {
     var indicators = indexedData[districtName] || {}
     for (var indicator in indicators) {
       document.getElementById('value-' + indicator).innerText = indicators[indicator]
     }
   }
-
+  // Normalize data string contents to avoid breaking charts
   function slugify (text) {
     return text.toString().toLowerCase().trim()
       .replace(/[^a-zA-Z0-9]/g, '-')  // Replace non-alphanumeric chars with -
       .replace(/\-\-+/g, '-')         // Replace multiple - with single -
       .replace(/^\-|\-$/i, '')        // Remove leading/trailing hyphen
   }
-
+  // Formats numeric values as percentages
   function percentify (value) {
     return (Math.round(value * 100) / 10) + '%'
   }
+  
 })()
