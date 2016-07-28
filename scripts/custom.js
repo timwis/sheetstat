ac0952fe7e07de40d20f5136d20c8c3b72e333ad
Karissa <k@karissademi.com>
2016-07-28 12:37:58 -0400

    Offset fix (#63)
    * Add wrapping div, move ids, remove unused classes
    
    * Remove unused classes
    
    * Add offset rules

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,8 +1,12 @@
 $(function() {
-	var h1 = $('#selectors').height()
+	var h1 = $('#selectors').outerHeight()
 	var h2 = $('.indicators-container').height()
 	$('#indicators').css({
 		'margin-top': h1,
 		'height': h2
 	})
+  $('.offset').css({
+    'margin-top': -h1,
+    'padding-top': h1
+  })
 })
